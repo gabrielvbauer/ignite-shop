90c7a88012e79b8dd5cd579b448c323baf8f8947
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-02-23 12:29:31 -0300

    feat: product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>{query.id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum ratione adipisci quo. Consequatur corporis quod, at aliquid ad perferendis placeat? Ducimus omnis assumenda est ullam suscipit corrupti nostrum, tempora voluptate!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
